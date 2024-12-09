6a9bf8394e7220f1c0ee53e0a1eaa90d51d9f525
DIO0550 <diot12345@yahoo.co.jp>
2024-12-09 23:34:28 +0900

    no message

--- react19-trial/app/routes/actionState/index.tsx
+++ react19-trial/app/routes/actionState/index.tsx
@@ -1,5 +1,95 @@
+import {
+  RegisterUserApi,
+  RegisterUserApiResponse,
+} from "../../apis/registerUser";
+import { useActionState, useRef } from "react";
+
+type State = {
+  registerId: string;
+  registerPassword: string;
+  isError: boolean;
+};
+
+const initialState: State = {
+  registerId: "",
+  registerPassword: "",
+  isError: false,
+};
+
+type Action = {
+  type: "register";
+  payload: {
+    id: string;
+    password: string;
+  };
+};
+
+const register = async (state: State, action: Action): Promise<State> => {
+  switch (action.type) {
+    case "register": {
+      try {
+        const response = await RegisterUserApi.Post({
+          id: action.payload.id,
+          password: action.payload.password,
+        });
+
+        const responseJson = (await response.json()) as RegisterUserApiResponse;
+
+        return {
+          ...state,
+          registerId: responseJson.id,
+          registerPassword: responseJson.password,
+          isError: false,
+        };
+      } catch {
+        return {
+          registerId: "",
+          registerPassword: "",
+          isError: true,
+        };
+      }
+    }
+    default:
+      return state;
+  }
+};
+
 const ActionState = () => {
-  return <div>ActionState</div>;
+  const [state, formAction, isPending] = useActionState(register, initialState);
+  const idRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <div>
+      {<div>{isPending ? "Loading" : "None"}</div>}
+
+      <label htmlFor="id">ID：</label>
+      <input ref={idRef} type="input" name="id" id="id" />
+
+      <label htmlFor="password">パスワード：</label>
+      <input ref={passwordRef} type="input" name="password" id="password" />
+
+      <button
+        onClick={() => {
+          formAction({
+            type: "register",
+            payload: {
+              id: idRef.current?.value ?? "",
+              password: passwordRef.current?.value ?? "",
+            },
+          });
+        }}
+      >
+        登録
+      </button>
+
+      <div>
+        <div>登録したid: {state.registerId}</div>
+        <div>登録したパスワード: {state.registerPassword}</div>
+        <div>エラー: {state.isError}</div>
+      </div>
+    </div>
+  );
 };
 
 export default ActionState;
